fe7582531f475d22dc70a7dcfa00120e19331f5a
jsorchik <jsorchik@gmail.com>
2012-02-11 22:09:44 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,10 +2,10 @@
 	$magicWord = 69;
 	$messageString = "DING DONG!"; //default value
 	
-	echo "<html><head></head><body>\n";
+echo "<html><head></head><body>\n";
 	foreach($_REQUEST as $key => $value){
-		if($key == "sendIt"){
-			$sendIt = $value;
+		if($key == "cmd"){
+			$cmd = $value;
 		}
 		if($key == "msg"){
 			$messageString = $value;
@@ -14,14 +14,14 @@
 	
 	$stamp = date('Y-m-d-Gis');
 	
-	if($sendIt == $magicWord){
+	if($cmd == $magicWord){
 		//send_mail("jsorchik@gmail.com", " Ding Dong", $messageString);
 	}
 	else{
-		echo "<p> sendIt=$sendIt string=$messageString date=$stamp </p>\n";
+echo "<p> cmd=$cmd string=$messageString date=$stamp </p>\n";
 	}
 	
-	echo "</body></html>\n";
+echo "</body></html>\n";
 	end;
 
 	Function send_mail($to, $subject, $message)
